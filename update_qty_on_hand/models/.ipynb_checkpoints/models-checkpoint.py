b94369852c9eace3aa406729c613101f524d29c9
tisoard <56162576+tisoard@users.noreply.github.com>
2019-10-18 18:48:10 +0000

    update_qty_on_hand

--- update_qty_on_hand/models/.ipynb_checkpoints/models-checkpoint.py
+++ update_qty_on_hand/models/.ipynb_checkpoints/models-checkpoint.py
@@ -2,8 +2,15 @@
 
 from odoo import models, fields, api
 
-# class update_qty_on_hand(models.Model):
-#     _name = 'update_qty_on_hand.update_qty_on_hand'
+class update_qty_on_hand(models.Model):
+    _name = 'update_qty_on_hand.update_qty_on_hand'
+    _inherit = 'product.product'
+    _inherit = 'stock.inventory'
+
+    # only keep records appearing in a bill of material (components, raw materials)
+    for records.filtered("bom_line_ids","!=",False):
+        inventory_quantity = 100
+
 
 #     name = fields.Char()
 #     value = fields.Integer()
